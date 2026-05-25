0c2c8cb2ad55530e7ec3d99ca5fa50366c08ba27
Neeraj Malik <neerajmalikak47@gmail.com>
2026-05-25 22:58:45 +0530

    Simple Print function for reusability.

---
+++ src/Others/PrintString/README.md
@@ -0,0 +1,5 @@
+This is a basic function of printing the string.
+
+I came to know that we can't use for each loop { Syntax : for( char ele : s) } in strings.
+
+So we have to go with the primitive for loop.
---
+++ src/Others/PrintString/printString.java
@@ -0,0 +1,12 @@
+package Others.PrintString;
+
+
+public class printString {
+    public void printString(String s){
+        for( int i = 0 ; i < s.length() ; i++ ){
+            System.out.print(s.charAt(i));
+        }
+        System.out.println();
+    }
+}
+
